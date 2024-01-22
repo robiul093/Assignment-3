beb3391c9075ad56db6bc6d166da173dabae3d1b
Robiul@Hasan <robihasan093@gmail.com>
2024-01-21 23:41:04 -0800

    complite banner and radiul section

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'regal-oi': '#243c5a',
+          'title': '#131318',
+          'para': '#FF0080',
+        },
+      }
+    }
+  }
